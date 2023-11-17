303e56474c59b9996f1a8ff2c5f69744dcee96e0
OliverYR03 <oliver_del22@hotmail.com>
2023-11-17 09:43:56 -0500

    Ala

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ def find_best_match(user_question: str, questions: List[str]) -> Optional[str]:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
     return matches[0] if matches else None
 
-def get_answer_for_mistake(question: str, knowledge_base: dict) -> str|None:
+def get_answer_for_mistake(question: str, knowledge_base: dict) -> str | None:
     for q in knowledge_base["ban"]:
         if q["ban"] == question:
             return q["bAnswer"]
